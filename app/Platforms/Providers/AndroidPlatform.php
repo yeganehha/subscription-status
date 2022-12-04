468a280d64c3aac02612129104d438608b644181
Erfan Ebrahimi <persionhost@gmail.com>
2022-12-04 18:10:21 +0330

    Add Platforms

---
+++ app/Platforms/Providers/AndroidPlatform.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Platforms\Providers;
+
+use App\Enums\StatusEnum;
+
+class AndroidPlatform extends \App\Platforms\Platform
+{
+    /**
+     * system check repeatedly status when get any error while check status.
+     * this method get numbers of seconds for delay and re-check
+     *Platforms\Providers
+     * @return int number of seconds for delay and re-check
+     */
+    public static function reCheckStatusOnErrorOccurred(): int
+    {
+        return parent::reCheckStatusOnErrorOccurred();
+    }
+
+    /**
+     * Process to check status of app in platform.
+     *
+     * @return StatusEnum
+     */
+    public function getStatus(): StatusEnum
+    {
+        $appUID = $this->app->uid;
+        return StatusEnum::Active;
+    }
+}
