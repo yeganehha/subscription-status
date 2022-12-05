e62a0e14f76ef557364ae5261973d0a99bd635b7
Erfan Ebrahimi <persionhost@gmail.com>
2022-12-06 03:00:32 +0330

    add help desk to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,81 @@
                         <strong>Graphql URI:</strong>
                         <a href="{{ route('graphql') }}" target="_blank">{{ route('graphql') }}</a>
                     </div>
+                    <hr>
+                    <h3>Restful:</h3>
+                    <div>
+                        <strong>Document:</strong>
+                        <a href="{{ route('l5-swagger.default.api') }}" target="_blank">{{ route('l5-swagger.default.api') }}</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card mt-2">
+                <div class="card-header">API Examples</div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <tr>
+                                <td>
+                                    <strong>Graphql</strong>
+                                    <a href="{{ route('graphql.graphiql') }}" target="_blank">graphiql</a>
+                                    <a href="{{ route('graphql') }}" target="_blank">Execute</a>
+                                    <div>
+                                        List subscription of special app
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Help Desk:</div>
+
+                <div class="card-body">
+                    <h3>Add New Platform:</h3>
+                    <div class="mx-2">
+                        For adding new platform provider just need to call:
+                        <blockquote class="bg-dark bg-opacity-25 p-2 mx-3 my-1" style="border-radius: 7px;">
+                            PHP artisan make:platform < name >
+                        </blockquote>
+                    </div>
+                    <hr>
+                    <h3>Run Check Subscription:</h3>
+                    <div class="mx-2">
+                        For Checking Subscription manually:
+                        <blockquote class="bg-dark bg-opacity-25 p-2 mx-3 my-1" style="border-radius: 7px;">
+                            PHP artisan queue:listen<br>
+                            PHP artisan subscription:check
+                        </blockquote>
+                    </div>
+                    <hr>
+                    <h3>Always Check Subscription:</h3>
+                    <div class="mx-2">
+                        Add These cronjobs to server to check subscription automatically:
+                        <blockquote class="bg-dark bg-opacity-25 p-2 mx-3 my-1" style="border-radius: 7px;">
+                            *	*	*	*	*	/usr/local/bin/php /laravel/artisan queue:work --stop-when-empty<br>
+                            0	0	*	*	*	/usr/local/bin/php /laravel/artisan subscription:check
+                        </blockquote>
+                    </div>
+                    <hr>
+                    <h3>Configuration:</h3>
+                    <div class="mx-2">
+                        For setting email configuration and choose day for run checking, open below file:
+                        <blockquote class="bg-dark bg-opacity-25 p-2 mx-3 my-1" style="border-radius: 7px;">
+                            config/subscription.php
+                        </blockquote>
+                        for config email server, change <strong>.env</strong> file.
+                    </div>
+                    <hr>
+                    <h3>Unit test:</h3>
+                    <div class="mx-2">
+                        for run test and get result of <strong>54 Unit Test</strong>, Just need call:
+                        <blockquote class="bg-dark bg-opacity-25 p-2 mx-3 my-1" style="border-radius: 7px;">
+                            php artisan test
+                        </blockquote>
+                    </div>
                 </div>
             </div>
         </div>
