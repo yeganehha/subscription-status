17ee91f9b53942f1ff96e0611a16279f5695c83d
Erfan Ebrahimi <persionhost@gmail.com>
2022-12-06 00:33:13 +0330

    restyling email

--- resources/views/emails/expired.blade.php
+++ resources/views/emails/expired.blade.php
@@ -1,8 +1,10 @@
-<div>
-List of Application that status change from "<strong>{{ $from_status }}</strong>" to "<strong>{{ $to_status }}</strong>" :
+<div style="margin-left: 25px; margin-top:150px; margin-right: 25px; ">
+    <div>
+        List of Application that status change from "<strong>{{ $from_status }}</strong>" to "<strong>{{ $to_status }}</strong>" :
+    </div>
+    <ul>
+        @foreach($apps as $app)
+            <li>{{ $app->name }} ({{ $app->uid }})</li>
+        @endforeach
+    </ul>
 </div>
-<ul>
-    @foreach($apps as $app)
-        <li>{{ $app->name }} ({{ $app->uid }})</li>
-    @endforeach
-</ul>
